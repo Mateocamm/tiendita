abcd618e616948e3b1dbf3a17cc493cd6d7a88aa
Mateo <mateocamm@gmail.com>
2021-08-18 17:37:42 +0800

    update order design

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,34 +1,61 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import db from "../../lib/firebase";
-import { Box, Heading, Stack, Text, Badge } from "@chakra-ui/react";
+import { FormatCurrency } from "../../lib/currency";
+import {
+  Box,
+  Heading,
+  Flex,
+  Text,
+  Badge,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+} from "@chakra-ui/react";
 
 function index({ order }) {
   return (
     <Layout>
-      <Box textAlign="center">
-        <Heading size="lg">Your Order Detail</Heading>
-        <Text>{order.orderId}</Text>
-        <Badge p="2" mb="4">
-          {order.status}
-        </Badge>
+      <Box>
+        <Flex justify="space-between" alignItems="center" mb="4">
+          <Box>
+            <Heading size="lg">Order Detail</Heading>
+            <Text>{order.orderId}</Text>
+          </Box>
+          <Badge p="2">{order.status}</Badge>
+        </Flex>
 
-        <Box mb="4">
-          <Text>
+        <Flex mb="4" direction="column" justify="flex-start">
+          <Text fontWeight="bold">
             {order.client.firstName} {order.client.lastName}
           </Text>
           <Text>{order.client.email}</Text>
           <Text>{order.client.phone}</Text>
+        </Flex>
+        <Box>
+          <Table  size="sm">
+            <Thead>
+              <Th pl="0">Item</Th>
+              <Th isNumeric>Price</Th>
+              <Th pr="0" isNumeric>Qty</Th>
+            </Thead>
+            <Tbody>
+              {order.items.map((item) => (
+                <Tr key={item.id}>
+                  <Td pl="0">{item.name}</Td>
+                  <Td isNumeric>{FormatCurrency(item.price)}</Td>
+                  <Td pr="0" isNumeric>{item.quantity}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+          <Box p="2" fontWeight="bold" textAlign="center">
+            Total: {FormatCurrency(order.total)}
+          </Box>
         </Box>
-        <Stack>
-          {order.items.map((item) => (
-            <Box key={item.id}>
-              <p>
-                {item.name} - {item.price} x {item.quantity}
-              </p>
-            </Box>
-          ))}
-        </Stack>
       </Box>
     </Layout>
   );
@@ -55,6 +82,9 @@ export async function getServerSideProps({ query }) {
         items: order.items,
         client: order.client,
         status: order.status ?? "",
+        total: order.items.reduce(function (total, item) {
+          return total + item.price * item.quantity;
+        }, 0),
       },
     }, // will be passed to the page component as props
   };
